7040fc06a53d815c074f51648313160649bf3ae8
kimthedrew <kimutai848@gmail.com>
2023-02-16 11:51:00 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
+/*printing*/
 int main() {
-    puts ("programming is like building a multilingual puzzle\n");
+    puts("programming is like building a multilingual puzzle\n");
     returns (0);
 }
