39f04803c6db5d5b3c1603d274bc4034c3192bc9
kimthedrew <kimutai848@gmail.com>
2023-02-16 17:20:52 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- *main - Entry point
- *
- *Description:'and that piece of art is useful" - Dora Korpar, 2015-10-19
- *
+ *main - A C program that prints a line to the standard error
  *Return:Always 0 (Success)
  */
-
 int main(void)
 {
-write(1."and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-
 /**
-*main -Entry point
-*return : Always 0
+*main - Entry point
+*
+*Description: 'programming is like building a multilingual puzzle'
+*
+*return: Always 0
 */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
-(returns)(0);
+ (returns)(0);
 }
