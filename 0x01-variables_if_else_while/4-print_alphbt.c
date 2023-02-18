b565d2d1914b19c07559c85d01a5e14d743a5a99
kimthedrew <kimutai848@gmail.com>
2023-02-17 18:19:24 -0800

    chosenones

---
+++ 0x01-variables_if_else_while/4-print_alphbt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main -Entry key of the program
+ *
+ *Description: prints alphabets except e and q
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if
+(letter != 'e' && letter != 'q');
+{
+putchar (letter);
+}
+}
+
+putchar('\n')
+return (0);
+}
