ef0cda1f37b87df8c1d75ca67a6b07706df0d7af
kimthedrew <kimutai848@gmail.com>
2023-02-18 12:10:13 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main -Entry key to the program
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0')
+}
+return (0);
+}
