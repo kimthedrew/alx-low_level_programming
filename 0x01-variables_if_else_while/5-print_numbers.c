67ac3c1d201cccaf02e4e4fb82a6738e5461592a
kimthedrew <kimutai848@gmail.com>
2023-02-18 02:20:50 -0800

    numbers010

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main -Entry key to the program
+ *
+ *Description: prints digits of base 10
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+char a;
+for (a = 0; a < 10; a++)
+{
+putchar(a)
+}
+putchar(n\);
+return (0);
+}
