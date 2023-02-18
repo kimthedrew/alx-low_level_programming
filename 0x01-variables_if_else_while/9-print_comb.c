83e2db6690b8638ac992b0d9a12b433f77b2563b
kimthedrew <kimutai848@gmail.com>
2023-02-18 15:08:06 -0800

    singledigits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main -Entry key for the program
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+int = a;
+for (a = 0; a <= 9; a++)
+{
+putchar(a + '0');
+if (a != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
