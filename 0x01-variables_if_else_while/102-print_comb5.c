75bc309dfdc075230173586444d9198d82cc2ec3
kimthedrew <kimutai848@gmail.com>
2023-02-19 05:13:20 -0800

    comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,17 +7,22 @@
 int main(void)
 {
 int a, b;
-for (a = 0; a <= 9; a++)
+for (a = 0; a < 100; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = a + 1; b < 100; b++)
 {
-putchar(a + '0');
-putchar(b + '0');
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
 {
-if (a != 9 || b != 9)
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+{
+if (a < 99 || b < 99)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
