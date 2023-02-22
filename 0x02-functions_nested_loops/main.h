40e9a5f2c2cf886338ff54e2b72b49b379315021
kimthedrew <kimutai848@gmail.com>
2023-02-22 13:21:27 -0800

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef main.h
+#define main.h
+
+int add(num1, num2);
+int subtract(num1, num2);
+int multiply(num1, num2);
+int divide(num1, num2);
+int print_hello(void);
+int print_name(char *name);
+int print_array(int *arr, size);
+int _putchar(char c);
+
+#endif
