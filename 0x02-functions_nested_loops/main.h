32777b514563f217d91efb0f9cc0a4063bcdc7d1
kimthedrew <kimutai848@gmail.com>
2023-02-23 02:54:43 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+/* function prototype */
 int add(int num1, int num2);
 int subtract(int num1, int num2);
 int multiply(int num1, int num2);
@@ -10,6 +11,8 @@ int divide(int num1, int num2);
 int print_hello(void);
 int print_name(char *name);
 int print_array(int *arr, int size);
+
+/* prototype for _putchar function */
 int _putchar(char c);
 
 #endif /* MAIN_H */
