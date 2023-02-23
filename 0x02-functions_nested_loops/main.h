00059517b9ecf76b6cb42072714ba494d67d0b51
kimthedrew <kimutai848@gmail.com>
2023-02-23 12:07:12 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int add(int a, int b);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 /* prototype for _putchar function */
 int _putchar(char c);
