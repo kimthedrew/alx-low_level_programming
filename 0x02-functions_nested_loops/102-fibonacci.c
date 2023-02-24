41740d310e388ff7f181f96c833ee0c4d80b8782
kimthedrew <kimutai848@gmail.com>
2023-02-24 11:15:09 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main -prints the first fifty fibonacci numbers
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+int b;
+unsigned long d, e, f;
+for (b = 0; b < 50; b++)
+{
+f = d + e;
+printf("%lu", f);
+
+d = e;
+f = e;
+
+if (b == 49)
+printf("\n");
+else
+printf(",");
+}
+return (0);
+}
