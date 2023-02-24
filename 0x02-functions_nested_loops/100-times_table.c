585a8f037694f281e63d17e77ba5afb34497e0e6
kimthedrew <kimutai848@gmail.com>
2023-02-24 02:43:46 -0800

    100times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,16 +20,16 @@ _putchar(b + '0');
 else if (c < 10 && b != 0)
 {
 _putchar(',');
-_putchar('.');
-_putchar('.');
-_putchar('.');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
 _putchar(c + '0');
 }
 else if (c >= 10 && c < 100)
 {
 _putchar(',');
-_putchar('.');
-_putchar('.');
+_putchar(' ');
+_putchar(' ');
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
 }
