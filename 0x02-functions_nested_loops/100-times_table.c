7bf1c9395793121e4432b565ef15e7192daffa33
kimthedrew <kimutai848@gmail.com>
2023-02-24 02:45:54 -0800

    100times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ _putchar((c % 10) + '0');
 else if (c >= 100)
 {
 _putchar(',');
-_putchar('.');
+_putchar(' ');
 _putchar((c / 100) + '0');
 _putchar(((c / 10) % 10) + '0');
 _putchar((c % 10) + '0');
