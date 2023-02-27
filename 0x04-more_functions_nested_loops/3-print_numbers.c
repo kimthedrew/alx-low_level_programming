ddd6637ba67e82cbb86c1096a12ed72da232c7ae
kimthedrew <kimutai848@gmail.com>
2023-02-27 14:05:09 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * print_numbers - prints numbers from 0 up t0 9
+ *
+ * Return: Numbers 0 up to 9
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+_putchar(a + '0');
+}
+_putchar('\n');
+}
