5bd511e713a33d3aa8efeef7455a5244c90521e9
kimthedrew <kimutai848@gmail.com>
2023-02-27 14:51:52 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,22 @@
  */
 void more_numbers(void);
 {
-	char n, c;
-	int i = 0;
+char n, c;
+int i = 0;
 
-	while (i < 10)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			c = n;
-			if (n > 9)
-			{
-				_putchar('1');
-				c = n % 10;
-			}
-			_putchar('0' + c);
-		}
-		_putchar('\n');
-		i++;
+while (i < 10)
+{
+for (n = 0; n <= 14; n++)
+{
+c = n;
+if (n > 9)
+{
+_putchar('1');
+c = n % 10;
+}
+_putchar('0' + c);
+}
+_putchar('\n');
+i++;
 }
 }
