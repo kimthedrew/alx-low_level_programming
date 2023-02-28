0ea3ae39207cb9e92497a6eff85ad076f031c1bd
kimthedrew <kimutai848@gmail.com>
2023-02-27 16:28:23 -0800

    8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints square followed by a new line
+ *  @n: number of _ to be printed
+ *  Return: 0
+ */
+void print_square(int n)
+{
+int c = 0, d;
+while (c < n && n > 0)
+{
+d = 0
+while (d < n)
+{
+putchar('#');
+d++;
+}
+_putchar('\n');
+c++;
+}
+if (c == 0)
+_putchar('\n');
+}
