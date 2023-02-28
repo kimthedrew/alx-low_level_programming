3510a55d31305e5da31fdc4b1155d289fc646465
kimthedrew <kimutai848@gmail.com>
2023-02-27 16:59:24 -0800

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Fizz Buzz code
+ *
+ * Return: void
+ */
+int main(void)
+{
+int a = 1;
+while (a <= 100)
+{
+if (a % 3 == 0)
+{
+printf("fizz");
+}
+else if (a % 5 == 0)
+{
+printf("buzz");
+}
+else if (a % 3 && a % 5 == 0)
+{
+printf("fizbuzz");
+}
+else
+{
+printf("%a", a);
+}
+if (a != 100)
+{
+putchar(' ');
+}
+a++
+}
+putchar('\n');
+return (0);
+}
