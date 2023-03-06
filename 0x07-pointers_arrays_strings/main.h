88f8569a0a7503a5bd46ebf1416a174f01fca5c6
kimthedrew <kimutai848@gmail.com>
2023-03-06 14:01:18 -0800

    mwmcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@
 #include <stddef.h>
 
 int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsinged int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
