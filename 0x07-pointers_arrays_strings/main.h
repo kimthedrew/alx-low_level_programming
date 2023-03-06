06575948d52cf40ac37c6564c98783bc0da1cf16
kimthedrew <kimutai848@gmail.com>
2023-03-06 13:53:54 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,7 +11,6 @@
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
